7fae2f853f7722d63708f86bccf6772a20f84b3b
Emir Hadzimahmutovic <emcapentest@gmail.com>
2021-12-11 18:02:03 +0100

    improvements

--- outputs.tf
+++ outputs.tf
@@ -2,14 +2,10 @@ output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
-output "public_subnets" {
-  value = zipmap(var.public_subnets[*].name, values(aws_subnet.public_subnets)[*].id)
-}
-
-output "private_subnets" {
-  value = zipmap(var.private_subnets[*].name, values(aws_subnet.private_subnets)[*].id)
+output "subnets" {
+  value = merge(zipmap(var.public_subnets[*].name, values(aws_subnet.public_subnets)[*].id), zipmap(var.private_subnets[*].name, values(aws_subnet.private_subnets)[*].id))
 }
 
 output "security_groups" {
-  value = zipmap(concat(keys(local.parent_groups), keys(local.child_groups)), concat(values(aws_security_group.sg_parent), values(aws_security_group.sg_child))[*].id)
+  value = zipmap(concat(keys(aws_security_group.depth_zero), keys(aws_security_group.depth_one), keys(aws_security_group.depth_two), keys(aws_security_group.depth_three)), concat(values(aws_security_group.depth_zero), values(aws_security_group.depth_one), values(aws_security_group.depth_two), values(aws_security_group.depth_three))[*].id)
 }
--- security_groups.tf
+++ security_groups.tf
@@ -1,28 +1,42 @@
-locals {
-  parent_groups = {for group in var.security_groups: group.name => group if length(flatten([for rule in concat(group.ingress, group.egress): rule.security_groups])) == 0}
-  child_groups = {for group in var.security_groups: group.name => group if lookup(local.parent_groups, group.name, null) == null}
-  applied_groups = {for group in values(aws_security_group.sg_parent)[*] : group.name => group.id} 
-}
-
-
-resource "aws_security_group" "sg_parent" {
-  for_each    = local.parent_groups
-  name        = each.value.name
+resource "aws_security_group" "depth_zero" {
+  for_each    = { for group in var.security_groups : group.name => group if group.depth == 0 }
+  name        = "${each.value.name}-${var.env}"
   description = each.value.description
   vpc_id      = aws_vpc.vpc.id
   ingress     = each.value.ingress
   egress      = each.value.egress
-  tags = each.value.tags != null ? merge(each.value.tags, {Environment = "${var.env}"}) : {Name = "security-group-${var.env}-${index(var.security_groups[*].name, each.value.name) + 1}"}
+  tags        = each.value.tags != null ? merge(each.value.tags, { Environment = "${var.env}" }) : { Name = "security-group-${var.env}-${index(var.security_groups[*].name, each.value.name) + 1}" }
 }
 
-resource "aws_security_group" "sg_child" {
-  for_each    = local.child_groups
-  name        = each.value.name
+resource "aws_security_group" "depth_one" {
+  for_each    = { for group in var.security_groups : group.name => group if group.depth == 1 }
+  name        = "${each.value.name}-${var.env}"
   description = each.value.description
   vpc_id      = aws_vpc.vpc.id
-  ingress     = length(each.value.ingress) == 0 ? each.value.ingress : [for rule in each.value.ingress: length(rule.security_groups) > 0 ? merge(rule, {security_groups: [for group in rule.security_groups: lookup(local.applied_groups, group)]}) : rule] 
-  egress      = length(each.value.egress) == 0 ? each.value.egress : [for rule in each.value.egress: length(rule.security_groups) > 0 ? merge(rule, {security_groups: [for group in rule.security_groups: lookup(local.applied_groups, group)]}) : rule] 
-  tags = each.value.tags != null ? merge(each.value.tags, {Environment = "${var.env}"}) : {Name = "security-group-${var.env}-${index(var.security_groups[*].name, each.value.name) + 1}"}
-  depends_on    = [aws_security_group.sg_parent]
+  ingress     = length(each.value.ingress) == 0 ? each.value.ingress : [for rule in each.value.ingress : length(rule.security_groups) > 0 ? merge(rule, { security_groups : [for group in rule.security_groups : lookup(aws_security_group.depth_zero, group).id] }) : rule]
+  egress      = length(each.value.egress) == 0 ? each.value.egress : [for rule in each.value.egress : length(rule.security_groups) > 0 ? merge(rule, { security_groups : [for group in rule.security_groups : lookup(aws_security_group.depth_zero, group).id] }) : rule]
+  tags        = each.value.tags != null ? merge(each.value.tags, { Environment = "${var.env}" }) : { Name = "security-group-${var.env}-${index(var.security_groups[*].name, each.value.name) + 1}" }
+  depends_on  = [aws_security_group.depth_zero]
 }
 
+resource "aws_security_group" "depth_two" {
+  for_each    = { for group in var.security_groups : group.name => group if group.depth == 2 }
+  name        = "${each.value.name}-${var.env}"
+  description = each.value.description
+  vpc_id      = aws_vpc.vpc.id
+  ingress     = length(each.value.ingress) == 0 ? each.value.ingress : [for rule in each.value.ingress : length(rule.security_groups) > 0 ? merge(rule, { security_groups : [for group in rule.security_groups : lookup(merge(aws_security_group.depth_zero, aws_security_group.depth_one), group).id] }) : rule]
+  egress      = length(each.value.egress) == 0 ? each.value.egress : [for rule in each.value.egress : length(rule.security_groups) > 0 ? merge(rule, { security_groups : [for group in rule.security_groups : lookup(merge(aws_security_group.depth_zero, aws_security_group.depth_one), group).id] }) : rule]
+  tags        = each.value.tags != null ? merge(each.value.tags, { Environment = "${var.env}" }) : { Name = "security-group-${var.env}-${index(var.security_groups[*].name, each.value.name) + 1}" }
+  depends_on  = [aws_security_group.depth_zero, aws_security_group.depth_one]
+}
+
+resource "aws_security_group" "depth_three" {
+  for_each    = { for group in var.security_groups : group.name => group if group.depth == 3 }
+  name        = "${each.value.name}-${var.env}"
+  description = each.value.description
+  vpc_id      = aws_vpc.vpc.id
+  ingress     = length(each.value.ingress) == 0 ? each.value.ingress : [for rule in each.value.ingress : length(rule.security_groups) > 0 ? merge(rule, { security_groups : [for group in rule.security_groups : lookup(merge(aws_security_group.depth_zero, aws_security_group.depth_one, aws_security_group.depth_two), group).id] }) : rule]
+  egress      = length(each.value.egress) == 0 ? each.value.egress : [for rule in each.value.egress : length(rule.security_groups) > 0 ? merge(rule, { security_groups : [for group in rule.security_groups : lookup(merge(aws_security_group.depth_zero, aws_security_group.depth_one, aws_security_group.depth_two), group).id] }) : rule]
+  tags        = each.value.tags != null ? merge(each.value.tags, { Environment = "${var.env}" }) : { Name = "security-group-${var.env}-${index(var.security_groups[*].name, each.value.name) + 1}" }
+  depends_on  = [aws_security_group.depth_zero, aws_security_group.depth_one, aws_security_group.depth_two]
+}
